f1dc11ac2a695b69236aab7979c52c2a96e5398d
Ano Joseph <anojoseph2011@gmail.com>
2025-06-21 21:45:50 +0530

    serial port issue

--- src/controllers/port/serialPortController.ts
+++ src/controllers/port/serialPortController.ts
@@ -1,12 +1,17 @@
 import { SerialPort } from 'serialport';
 import { Request, Response } from 'express';
+import os from 'os';
 
 export const getSerialPorts = async (req: Request, res: Response) => {
     try {
+        // Avoid trying to list ports in production or non-supported environments
+        if (os.platform() === 'linux' && process.env.NODE_ENV === 'production') {
+            return res.status(200).json({ message: 'SerialPort listing is disabled in production.' });
+        }
+
         const ports = await SerialPort.list();
         res.json(ports);
-    } catch (error) {
-        res.status(500).json({ message: 'Unable to fetch Serial Ports!', error });
+    } catch (error:any) {
+        res.status(500).json({ message: 'Unable to fetch Serial Ports!', error: error.message });
     }
 };
-
